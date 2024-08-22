9b92748e9af9ca111317aa1acc66e5bcc59c1e8a
karenbeltranpereyra <karenbeltran.digital@gmail.com>
2024-08-22 00:19:37 -0600

    Add files via upload

---
+++ JS/portfolio.js
@@ -0,0 +1,447 @@
+const tiAndRssData = {
+  es: [{
+    title: "TI",
+    description: "Gestioné y coordiné de principio a fin los siguientes micrositios y landings, para otros también he coordinado el mantenimiento y actualizaciones.",
+    sections: [
+      {
+        brand: "OXXO",
+        logo: "assets/brands/Oxxo.png",
+        links: [
+          {
+            link: "https://www.oxxo.com/recetario",
+            title: "Recetario OXXO",
+            image: "assets/portfolio/recetarioOxxo.webp"
+          },
+          {
+            link: "https://www.oxxo.com/mipymes",
+            title: "Mi Pymes OXXO",
+            image: "assets/portfolio/pymesOxxo.webp"
+          },
+          {
+            link: "https://www.oxxo.com/envia-y-recibe-dinero",
+            title: "Envía y Recibe Dinero OXXO",
+            image: "assets/portfolio/enviaRecibeOxxo.webp"
+          },
+          {
+            link: "https://www.oxxo.com/pagos-depositos-tarjetas",
+            title: "Pagos y Depósitos",
+            image: "assets/portfolio/pagosDepositosOxxo.webp"
+          },
+          {
+            link: "https://www.oxxo.com/sostenibilidad",
+            title: "Sostenibilidad OXXO",
+            image: "assets/portfolio/sostenibilidadOxxo.webp"
+          },
+          {
+            link: "https://www.oxxo.com/reunion",
+            title: "Reunión OXXO",
+            image: "assets/portfolio/reunionOxxo.webp"
+          }
+        ]
+      },
+      {
+        brand: "andatti",
+        logo: "assets/brands/Oxxo.png",
+        links: [
+          {
+            link: "https://www.oxxoandatti.com/",
+            title: "andatti",
+            image: "assets/portfolio/andattiOxxo.webp"
+          }
+        ]
+      },
+      {
+        brand: "MABE",
+        logo: "assets/brands/mabe.png",
+        links: [
+          {
+            link: "https://mabeglobal.com/es_MX/",
+            title: "MABE Global",
+            image: "assets/portfolio/mabeGlobal.webp"
+          }
+        ]
+      },
+      {
+        brand: "MERZ AESTHETICS",
+        logo: "assets/brands/Merz.png",
+        links: [
+          {
+            link: "https://merzaesthetics.com/",
+            title: "Merz Aesthetics",
+            image: "assets/portfolio/merzAesthetics.webp"
+          }
+        ]
+      }
+    ]
+  }
+    ,
+    {
+      title: "RSS",
+      description: "De las siguientes marcas llevé la gestión, coordinación, asignación de recursos, control de presupuestos, seguimientos de cronogramas, visibilidad a ejecutivo de cuenta para la creación de contenidos de todas sus redes sociales, gestionando también altas producciones, mini producciones, coberturas de eventos y festivales, desarrollo de materiales de principio a fin para cada red, apegandome a una estrategia enfocada a ciertos objetivos y resultados de acuerdo la marca.",
+      sections: [
+        {
+          brand: "OXXO",
+          logo: "assets/brands/Oxxo.png",
+          links: [
+            {
+              link: "https://www.facebook.com/OXXOTiendas",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/tiendasoxxo/",
+              icon: "assets/instagram.png"
+            },
+            {
+              link: "https://www.tiktok.com/@tiendasoxxo",
+              icon: "assets/tiktok.svg"
+            },
+            {
+              link: "https://www.linkedin.com/company/oxxo/",
+              icon: "assets/linkedin.png"
+            }
+          ]
+        },
+        {
+          brand: "NESTEA",
+          logo: "assets/brands/Nestea.png",
+          links: [
+            {
+              link: "https://www.facebook.com/NESTEA.Mexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/nesteamx/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "TAURUS",
+          logo: "assets/brands/Taurus.png",
+          links: [
+            {
+              link: "https://www.facebook.com/taurusmexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/taurus_mexico/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "BIOELECTRONICS",
+          logo: "assets/brands/Bioelectronics.png",
+          links: [
+            {
+              link: "https://www.facebook.com/BioelectronicsMexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/bioelectronics_/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "CONAIR",
+          logo: "assets/brands/Conair.png",
+          links: [
+            {
+              link: "https://www.facebook.com/conairmexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/conairmexico/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "CUISINART",
+          logo: "assets/brands/Cuisinart.png",
+          links: [
+            {
+              link: "https://www.facebook.com/cuisinart",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/cuisinart/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        }
+      ]
+    }
+    
+  ],
+  en: [
+    {
+      title: "IT",
+      description: "I managed and coordinated the following microsites and landings from start to finish, and for others, I also coordinated maintenance and updates.",
+      sections: [
+        {
+          brand: "OXXO",
+          logo: "assets/brands/Oxxo.png",
+          links: [
+            {
+              link: "https://www.oxxo.com/recetario",
+              title: "Recetario OXXO",
+              image: "assets/portfolio/recetarioOxxo.webp"
+            },
+            {
+              link: "https://www.oxxo.com/mipymes",
+              title: "Mi Pymes OXXO",
+              image: "assets/portfolio/pymesOxxo.webp"
+            },
+            {
+              link: "https://www.oxxo.com/envia-y-recibe-dinero",
+              title: "Envía y Recibe Dinero OXXO",
+              image: "assets/portfolio/enviaRecibeOxxo.webp"
+            },
+            {
+              link: "https://www.oxxo.com/pagos-depositos-tarjetas",
+              title: "Pagos y Depósitos",
+              image: "assets/portfolio/pagosDepositosOxxo.webp"
+            },
+            {
+              link: "https://www.oxxo.com/sostenibilidad",
+              title: "Sostenibilidad OXXO",
+              image: "assets/portfolio/sostenibilidadOxxo.webp"
+            },
+            {
+              link: "https://www.oxxo.com/reunion",
+              title: "Reunión OXXO",
+              image: "assets/portfolio/reunionOxxo.webp"
+            }
+          ]
+        },
+        {
+          brand: "andatti",
+          logo: "assets/brands/Oxxo.png",
+          links: [
+            {
+              link: "https://www.oxxoandatti.com/",
+              title: "andatti",
+              image: "assets/portfolio/andattiOxxo.webp"
+            }
+          ]
+        },
+        {
+          brand: "MABE",
+          logo: "assets/brands/mabe.png",
+          links: [
+            {
+              link: "https://mabeglobal.com/es_MX/",
+              title: "MABE Global",
+              image: "assets/portfolio/mabeGlobal.webp"
+            }
+          ]
+        },
+        {
+          brand: "MERZ AESTHETICS",
+          logo: "assets/brands/Merz.png",
+          links: [
+            {
+              link: "https://merzaesthetics.com/",
+              title: "Merz Aesthetics",
+              image: "assets/portfolio/merzAesthetics.webp"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "RSS",
+      description: "I managed the planning, coordination, resource allocation, budget control, timeline tracking, and provided visibility to the account executive for content creation across all social media channels of the following brands. Additionally, I handled large-scale productions, mini-productions, event and festival coverage, and developed materials from start to finish for each platform, adhering to a strategy focused on specific objectives and brand-aligned results.",
+      sections: [
+        {
+          brand: "OXXO",
+          logo: "assets/brands/Oxxo.png",
+          links: [
+            {
+              link: "https://www.facebook.com/OXXOTiendas",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/tiendasoxxo/",
+              icon: "assets/instagram.png"
+            },
+            {
+              link: "https://www.tiktok.com/@tiendasoxxo",
+              icon: "assets/tiktok.svg"
+            },
+            {
+              link: "https://www.linkedin.com/company/oxxo/",
+              icon: "assets/linkedin.png"
+            }
+          ]
+        },
+        {
+          brand: "NESTEA",
+          logo: "assets/brands/Nestea.png",
+          links: [
+            {
+              link: "https://www.facebook.com/NESTEA.Mexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/nesteamx/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "TAURUS",
+          logo: "assets/brands/Taurus.png",
+          links: [
+            {
+              link: "https://www.facebook.com/taurusmexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/taurus_mexico/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "BIOELECTRONICS",
+          logo: "assets/brands/Bioelectronics.png",
+          links: [
+            {
+              link: "https://www.facebook.com/BioelectronicsMexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/bioelectronics_/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "CONAIR",
+          logo: "assets/brands/Conair.png",
+          links: [
+            {
+              link: "https://www.facebook.com/conairmexico",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/conairmexico/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        },
+        {
+          brand: "CUISINART",
+          logo: "assets/brands/Cuisinart.png",
+          links: [
+            {
+              link: "https://www.facebook.com/cuisinart",
+              icon: "assets/facebook.png"
+            },
+            {
+              link: "https://www.instagram.com/cuisinart/",
+              icon: "assets/instagram.png"
+            }
+          ]
+        }
+      ]
+    }
+    
+  ]
+};
+
+
+function generateTiAndRssHTML(language = 'es') {
+  const container = document.getElementById('ti-and-rss');
+  container.innerHTML = '';
+
+  const data = tiAndRssData[language];
+
+  data.forEach(sectionData => {
+    const sectionDiv = document.createElement('div');
+    sectionDiv.classList.add('portfolio-container');
+
+    const sectionTitle = document.createElement('h2');
+    sectionTitle.textContent = sectionData.title;
+    sectionDiv.appendChild(sectionTitle);
+
+    const sectionDescription = document.createElement('p');
+    sectionDescription.textContent = sectionData.description;
+    sectionDiv.appendChild(sectionDescription);
+
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('portfolioCard-container');
+
+    sectionData.sections.forEach(brandData => {
+      if (sectionData.title === "TI" || sectionData.title === "IT") {
+        brandData.links.forEach(linkData => {
+          const portfolioCard = document.createElement('div');
+          portfolioCard.classList.add('portfolioCard');
+
+          const img = document.createElement('img');
+          img.src = linkData.image;
+          img.alt = linkData.title;
+          portfolioCard.appendChild(img);
+
+          const cardTitle = document.createElement('h3');
+          cardTitle.textContent = linkData.title;
+          portfolioCard.appendChild(cardTitle);
+
+          portfolioCard.addEventListener('click', () => {
+            window.open(linkData.link, '_blank');
+          });
+
+          cardContainer.appendChild(portfolioCard);
+        });
+      } else {
+        const portfolioCard = document.createElement('div');
+  portfolioCard.classList.add('portfolioCardRSS');
+
+  const img = document.createElement('img');
+  img.src = brandData.logo;
+  img.alt = brandData.brand;
+  img.classList.add('back')
+  portfolioCard.appendChild(img);
+
+  const linksContainer = document.createElement('div');
+  linksContainer.classList.add('links-container');
+
+  brandData.links.forEach(linkData => {
+    const iconAnchor = document.createElement('a');
+    iconAnchor.href = linkData.link;
+    iconAnchor.target = '_blank';
+
+    const iconImg = document.createElement('img');
+    iconImg.src = linkData.icon;
+    iconImg.alt = linkData.link.split('/')[2];
+    
+    iconAnchor.appendChild(iconImg);
+    linksContainer.appendChild(iconAnchor);
+  });
+
+  portfolioCard.appendChild(linksContainer);
+  cardContainer.appendChild(portfolioCard);
+
+      }
+    });
+
+    sectionDiv.appendChild(cardContainer);
+    container.appendChild(sectionDiv);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  generateTiAndRssHTML();
+
+  document.getElementById('navbar-container').addEventListener('click', (event) => {
+    if (event.target.id === 'language-toggle') {
+      const newLanguage = event.target.textContent === 'Español' ? 'es' : 'en';
+      generateTiAndRssHTML(newLanguage);
+    }
+  });
+});
+
+
