30af5a4cb910b70025b988b43c7e614beccc652a
karenbeltranpereyra <karenbeltran.digital@gmail.com>
2025-02-09 12:45:20 -0600

    Update experience.js

--- JS/experience.js
+++ JS/experience.js
@@ -26,81 +26,92 @@ const experienceData = {
       "period": "2022 - 2024",
       "position": "PROJECT MANAGER",
       "activities": [
-        "Coordination of media activities",
-        "Internal communication",
-        "Project organization",
-        "Specify the quality of each deliverable part",
-        "Estimate resources and timelines needed",
-        "Develop and implement a plan and processes for the project",
-        "Risk management, issues and changes in the project",
-        "Monitor progress and processes",
-        "Maintain continuous communication with stakeholders",
-        "Supplier management"
-      ]
+  "Communication with stakeholders",
+  "Management of simultaneous projects, including website developments",
+  "Development of project plans, including scope, goals, deliverables, timelines, and budgets",
+  "Coordinate and manage resources for each project",
+  "Coordination of internal and external teams (Content, social, media, IT, design, animation, production, and creatives)",
+  "Ensure all team members understand their roles and responsibilities",
+  "Conduct status meetings and periodic follow-ups to ensure progress on active tickets or risk resolution",
+  "Provide clear direction and guidance to team members",
+  "Monitor project progress and make adjustments as necessary to ensure successful delivery within established timelines and budgets",
+  "Communicate project status, updates, and risks to stakeholders and senior management",
+  "Collaborate with multidisciplinary teams to ensure effective project coordination and alignment with organizational objectives",
+  "Foster a collaborative and positive work environment, promoting teamwork and open communication",
+  "Ensure compliance with best practices in project management, methodologies, and quality standards",
+  "Conduct post-project evaluations to identify areas for improvement",
+  "Supplier management",
+  "Management of large and small productions",
+  "Development of quotations"
+]
+
     },
     {
       "company": "COCO MARKETING",
       "period": "2019-2022",
       "position": "ACCOUNT EXECUTIVE | PROJECT MANAGER",
       "activities": [
-        "Management of premium projects",
-        "Social media containment",
-        "Content supervision",
-        "Style correction",
-        "Communication plans and strategies as well as participation in their development",
-        "Recording and editing management",
-        "Management and follow-up of website development (small, medium, and large)",
-        "Email campaign management and participation in the process",
-        "Direct client interaction",
-        "Organization and supervision of the agency's internal team",
-        "Supervision and activation of social media campaigns (Facebook Ads, Google AdWords)",
-        "Decision-making, task schemes, timetable with client, internal team and implementation of solutions",
-        "Development of reports and analysis of monthly or project metrics"
-      ]
+  "Communication with stakeholders",
+  "Premium project management",
+  "Social media support",
+  "Content supervision",
+  "Style correction",
+  "Communication plans and strategies, as well as participation in their development",
+  "Management of recordings and editing",
+  "Management and follow-up of the development of small, medium, and large websites",
+  "Management and execution of mailings, as well as participation in the process",
+  "Direct interaction with clients",
+  "Organization and supervision of the agency's internal team",
+  "Supervision and activation of social media campaigns, Facebook Ads, Google AdWords",
+  "Decision-making, task schemes, timetable coordination with clients, internal teams, and solution implementation",
+  "Development of reports and analysis of metrics monthly or by project"
+]
+
     },
     {
       "company": "LATENTE STUDIO",
       "period": "2019",
       "position": "ACCOUNT EXECUTIVE | PROJECT MANAGER",
       "activities": [
-        "Media and social media positioning strategy",
-        "Coordination of media activities and advertising",
-        "Internal communication: text writing, style correction, content design",
-        "Event organization (conferences, workshops, presentations, etc.)",
-        "Social media strategy and updates: AdWords and Google Analytics",
-        "Ad design and publishing",
-        "Public relations",
-        "Organization and direction of photo shoots"
-      ]
+  "Media and social media positioning strategy",
+  "Coordination of media activity and advertising campaigns",
+  "Internal communication: text writing, style correction, and content design",
+  "Event organization (Conferences, workshops, presentations, etc.)",
+  "Social media updates and strategy: Adwords and Google Analytics",
+  "Design and publication of advertisements",
+  "Public relations",
+  "Organization and direction of photo and video shootings"
+]
     },
     {
       "company": "PAUTA CREATIVA MARKETING AGENCY",
       "period": "2018-2019",
       "position": "COMMUNITY MANAGER",
       "activities": [
-        "Media and social media positioning strategy",
-        "Coordination of media activities and advertising",
-        "Internal communication: text writing, style correction, content design",
-        "Event organization (conferences, workshops, presentations, etc.)",
-        "Social media strategy and updates: AdWords and Google Analytics",
-        "Ad design and publishing",
-        "Public relations",
-        "Organization and direction of photo shoots"
-      ]
+  "Media and social media positioning strategy",
+  "Coordination of media activity and advertising campaigns",
+  "Internal communication: text writing, style correction, content design",
+  "Event organization (Conferences, workshops, presentations, etc.)",
+  "Social media updates and strategy: Adwords and Google Analytics",
+  "Design and publication of advertisements",
+  "Public relations",
+  "Organization and direction of photo and video shootings"
+]
+
     },
     {
       "company": "ROEL COMERCIALIZADORA",
       "period": "2015-2017",
       "position": "COMMUNITY MANAGER",
       "activities": [
-        "Account management for brands like Canson, Cretacolor, Milan, Acrilex, Pintart, Estambres Circulo, Indart, Condor",
-        "Client follow-up",
-        "Creation and monitoring of advertising campaigns",
-        "Social media containment",
-        "Report generation",
-        "Video recording for content",
-        "Management and direction of online broadcasts"
-      ]
+  "Account management for clients such as Canson, Cretacolor, Milan, Acrilex, Pintart, Estambres Circulo, Indart, and Condor",
+  "Client follow-up",
+  "Creation and monitoring of advertising campaigns",
+  "Social media support",
+  "Generation of reports",
+  "Recording videos for content",
+  "Management and direction of online broadcasts"
+]
     }
   ],
   es: [
@@ -130,81 +141,94 @@ const experienceData = {
       "period": "2022 - 2024",
       "position": "PROJECT MANAGER",
       "activities": [
-        "Coordinación de actividades de medios",
-        "Comunicación interna",
-        "Organización de proyectos",
-        "Especificar la calidad de cada parte entregable",
-        "Estimación de recursos y tiempos necesarios",
-        "Desarrollar e implementar un plan y procesos para el proyecto",
-        "Gestión de riesgos, problemas y cambios en el proyecto",
-        "Monitorear el progreso y los procesos",
-        "Mantener comunicación continua con las partes interesadas",
-        "Gestión de proveedores"
-      ]
+  "Comunicación con stakeholders",
+  "Manejo de proyectos en simultáneo, incluyendo desarrollos de sitio web",
+  "Desarrollo de planes de proyecto, incluyendo alcance, metas, entregables, cronogramas y presupuestos",
+  "Coordinar y gestionar los recursos para cada proyecto",
+  "Coordinación de equipos internos y externos (Content, social, medios, TI, diseño, animación, producción y creativos)",
+  "Asegurar que todos los miembros del equipo comprendan sus roles y responsabilidades",
+  "Realización de reuniones de estatus y seguimientos periódicos para asegurar el avance correcto de los tickets activos o resolución de riesgos",
+  "Proporcionar dirección clara y orientación a los miembros del equipo de proyecto",
+  "Supervisar el progreso del proyecto y realizar ajustes según sea necesario para garantizar la entrega exitosa dentro de los plazos y presupuestos establecidos",
+  "Comunicar el estado del proyecto, actualizaciones y riesgos a las partes interesadas y a la alta dirección",
+  "Colaborar con equipos multidisciplinarios para asegurar una coordinación efectiva del proyecto y su alineación con los objetivos organizacionales",
+  "Fomentar un entorno de trabajo colaborativo y positivo, promoviendo el trabajo en equipo y la comunicación abierta",
+  "Garantizar el cumplimiento de las mejores prácticas de gestión de proyectos, metodologías y estándares de calidad",
+  "Realizar evaluaciones posteriores al proyecto e identificar áreas de mejora",
+  "Gestión de proveedores",
+  "Gestión de producciones grandes y pequeñas",
+  "Desarrollo de cotizaciones"
+]
+
     },
     {
       "company": "COCO MARKETING",
       "period": "2019-2022",
       "position": "EJECUTIVO DE CUENTAS | PROJECT MANAGER",
       "activities": [
-        "Gestión de proyectos premium",
-        "Contención en redes sociales",
-        "Supervisión de contenido",
-        "Corrección de estilo",
-        "Planes de comunicación y estrategias, así como participación en su desarrollo",
-        "Gestión de grabación y edición",
-        "Gestión y seguimiento del desarrollo de sitios web (pequeños, medianos y grandes)",
-        "Gestión de campañas de correo electrónico y participación en el proceso",
-        "Interacción directa con el cliente",
-        "Organización y supervisión del equipo interno de la agencia",
-        "Supervisión y activación de campañas en redes sociales (Facebook Ads, Google AdWords)",
-        "Toma de decisiones, esquemas de tareas, cronograma con el cliente, equipo interno y implementación de soluciones",
-        "Desarrollo de informes y análisis de métricas mensuales o de proyectos"
-      ]
+  "Comunicación con stakeholders",
+  "Gestión de proyectos premium",
+  "Contención de RR. SS.",
+  "Supervisión de contenidos",
+  "Corrección de estilo",
+  "Planes de comunicación y estrategias así como participación en la elaboración de los mismos",
+  "Gestión de grabaciones y ediciones",
+  "Gestión y seguimiento del desarrollo de sitios web, pequeños, medianos y grandes",
+  "Gestión de envíos de mailings así como participación en el proceso",
+  "Trato directo con el cliente",
+  "Organización y supervisión de equipo interno de la agencia",
+  "Supervisión y activación de campañas en redes sociales, Facebook Ads, Google AdWords",
+  "Toma de decisiones, esquemas de tareas, timetable con el cliente, el equipo interno e implementación de soluciones",
+  "Desarrollo de reportes y análisis de métricas mensuales o por proyecto"
+]
+
     },
     {
       "company": "LATENTE STUDIO",
       "period": "2019",
       "position": "EJECUTIVO DE CUENTAS | PROJECT MANAGER",
       "activities": [
-        "Estrategia de posicionamiento en medios y redes sociales",
-        "Coordinación de actividades de medios y publicidad",
-        "Comunicación interna: redacción de textos, corrección de estilo, diseño de contenido",
-        "Organización de eventos (conferencias, talleres, presentaciones, etc.)",
-        "Estrategia y actualizaciones en redes sociales: AdWords y Google Analytics",
-        "Diseño y publicación de anuncios",
-        "Relaciones públicas",
-        "Organización y dirección de sesiones fotográficas"
-      ]
+  "Estrategia de posicionamiento en medios y RR. SS.",
+  "Coordinación de actividad en medios y pauta publicitaria",
+  "Comunicación interna: redacción de textos, corrección de estilo y diseño de contenido",
+  "Organización de eventos (Conferencias, talleres, presentaciones, etc.)",
+  "Actualización y estrategia en Social Media: Adwords y Google Analytics",
+  "Diseño y publicación de anuncios",
+  "Relaciones públicas",
+  "Organización y dirección de shootings"
+]
+
+
     },
     {
       "company": "PAUTA CREATIVA AGENCIA DE MARKETING",
       "period": "2018-2019",
       "position": "COMMUNITY MANAGER",
       "activities": [
-        "Estrategia de posicionamiento en medios y redes sociales",
-        "Coordinación de actividades de medios y publicidad",
-        "Comunicación interna: redacción de textos, corrección de estilo, diseño de contenido",
-        "Organización de eventos (conferencias, talleres, presentaciones, etc.)",
-        "Estrategia y actualizaciones en redes sociales: AdWords y Google Analytics",
-        "Diseño y publicación de anuncios",
-        "Relaciones públicas",
-        "Organización y dirección de sesiones fotográficas"
-      ]
+  "Estrategia de posicionamiento en medios y RR. SS.",
+  "Coordinación de actividad en medios y pauta publicitaria",
+  "Comunicación interna: redacción de textos, corrección de estilo, diseño de contenido",
+  "Organización de eventos (Conferencias, talleres, presentaciones, etc.)",
+  "Actualización y estrategia en Social Media: Adwords y Google Analytics",
+  "Diseño y publicación de anuncios",
+  "Relaciones Públicas",
+  "Organización y dirección de shootings"
+]
+
     },
     {
       "company": "ROEL COMERCIALIZADORA",
       "period": "2015-2017",
       "position": "COMMUNITY MANAGER",
       "activities": [
-        "Gestión de cuentas para marcas como Canson, Cretacolor, Milan, Acrilex, Pintart, Estambres Circulo, Indart, Condor",
-        "Seguimiento al cliente",
-        "Creación y monitoreo de campañas publicitarias",
-        "Contención en redes sociales",
-        "Generación de reportes",
-        "Grabación de video para contenido",
-        "Gestión y dirección de transmisiones en línea"
-      ]
+  "Gestión de cuentas como Canson, Cretacolor, Milan, Acrilex, Pintart, Estambres Circulo, Indart, Condor",
+  "Seguimiento a cliente",
+  "Creación y monitoreo de campañas publicitarias",
+  "Contención de RR. SS.",
+  "Generación de reportes",
+  "Grabación de vídeos para contenidos",
+  "Gestión y dirección de transmisión online"
+]
     }
   ]
 };
